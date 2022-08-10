0c1cb821833921dd3f8af91934284a75541e1e9e
FadiFutainah <fadiftenaft@gmail.com>
2022-08-10 21:16:01 +0300

    add data providers

---
+++ lib/data/providers/profile_provider.dart
@@ -0,0 +1,38 @@
+import 'package:app/data/helpers/restapi_service.dart';
+import 'package:app/data/models/profile_dto.dart';
+
+class ProfileProvider {
+  final RestApiService _restApi =
+      RestApiService('http://127.0.0.1:8088/api/student-profile/');
+
+  Future<List<ProfileDto>> getProfiles() async {
+    try {
+      var response = await _restApi.get('profiles');
+      return List.from(response.map((e) => ProfileDto.fromMap(e)));
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<ProfileDto> getMyProfile() async {
+    try {
+      var response = await _restApi.get('profile/me', hasToken: true);
+      return ProfileDto.fromMap(response);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<ProfileDto> getProfile(int id) async {
+    try {
+      var response = await _restApi.get('profile/$id');
+      return ProfileDto.fromMap(response);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future editProfile() {
+    throw UnimplementedError();
+  }
+}
