18042d6b62c66f879a2e6decef2210506f4373df
fadiftenaft <fadiftenaft@gmail.com>
2022-08-16 09:12:53 +0300

    generate autoroutes

--- lib/application/utils/routes/router.dart
+++ lib/application/utils/routes/router.dart
@@ -1,6 +1,9 @@
+import 'package:app/application/features/add_forum/view/add_forum_page.dart';
 import 'package:app/application/features/authentication/view/splash_page.dart';
+import 'package:app/application/features/forum/view/forum_page.dart';
 import 'package:app/application/features/home/view/home_page.dart';
-import 'package:app/application/features/profile/view/foreign_profile_page.dart';
+import 'package:app/application/features/login/view/login_page.dart';
+import 'package:app/application/features/foreign_profile/view/foreign_profile_page.dart';
 import 'package:app/application/features/profile/view/my_profile_page.dart';
 import 'package:auto_route/annotations.dart';
 
@@ -8,9 +11,12 @@ import 'package:auto_route/annotations.dart';
   replaceInRouteName: 'Page,Route',
   routes: <AutoRoute>[
     AutoRoute(page: SplashPage, initial: true),
+    AutoRoute(page: LoginPage),
     AutoRoute(page: HomePage),
     AutoRoute(page: MyProfilePage),
     AutoRoute(page: ForeignProfilePage),
+    AutoRoute(page: ForumPage),
+    AutoRoute(page: AddForumPage),
   ],
 )
 class $AppRouter {}
--- lib/application/utils/routes/router.gr.dart
+++ lib/application/utils/routes/router.gr.dart
@@ -11,78 +11,152 @@
 // ignore_for_file: type=lint
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:app/application/features/add_forum/view/add_forum_page.dart'
+    as _i7;
 import 'package:app/application/features/authentication/view/splash_page.dart'
     as _i1;
-import 'package:app/application/features/home/view/home_page.dart' as _i2;
-import 'package:app/application/features/profile/view/foreign_profile_page.dart'
-    as _i4;
+import 'package:app/application/features/foreign_profile/view/foreign_profile_page.dart'
+    as _i5;
+import 'package:app/application/features/forum/view/forum_page.dart' as _i6;
+import 'package:app/application/features/home/view/home_page.dart' as _i3;
+import 'package:app/application/features/login/view/login_page.dart' as _i2;
 import 'package:app/application/features/profile/view/my_profile_page.dart'
-    as _i3;
-import 'package:auto_route/auto_route.dart' as _i5;
-import 'package:flutter/material.dart' as _i6;
+    as _i4;
+import 'package:auto_route/auto_route.dart' as _i8;
+import 'package:flutter/material.dart' as _i9;
 
-class AppRouter extends _i5.RootStackRouter {
-  AppRouter([_i6.GlobalKey<_i6.NavigatorState>? navigatorKey])
+class AppRouter extends _i8.RootStackRouter {
+  AppRouter([_i9.GlobalKey<_i9.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i5.PageFactory> pagesMap = {
+  final Map<String, _i8.PageFactory> pagesMap = {
     SplashRoute.name: (routeData) {
-      return _i5.AdaptivePage<dynamic>(
+      return _i8.AdaptivePage<dynamic>(
           routeData: routeData, child: const _i1.SplashPage());
     },
+    LoginRoute.name: (routeData) {
+      return _i8.AdaptivePage<dynamic>(
+          routeData: routeData, child: const _i2.LoginPage());
+    },
     HomeRoute.name: (routeData) {
-      return _i5.AdaptivePage<dynamic>(
-          routeData: routeData, child: const _i2.HomePage());
+      final args =
+          routeData.argsAs<HomeRouteArgs>(orElse: () => const HomeRouteArgs());
+      return _i8.AdaptivePage<dynamic>(
+          routeData: routeData, child: _i3.HomePage(key: args.key));
     },
     MyProfileRoute.name: (routeData) {
-      return _i5.AdaptivePage<dynamic>(
-          routeData: routeData, child: const _i3.MyProfilePage());
+      return _i8.AdaptivePage<dynamic>(
+          routeData: routeData, child: const _i4.MyProfilePage());
     },
     ForeignProfileRoute.name: (routeData) {
-      return _i5.AdaptivePage<dynamic>(
-          routeData: routeData, child: const _i4.ForeignProfilePage());
+      return _i8.AdaptivePage<dynamic>(
+          routeData: routeData, child: const _i5.ForeignProfilePage());
+    },
+    ForumRoute.name: (routeData) {
+      final args = routeData.argsAs<ForumRouteArgs>(
+          orElse: () => const ForumRouteArgs());
+      return _i8.AdaptivePage<dynamic>(
+          routeData: routeData, child: _i6.ForumPage(key: args.key));
+    },
+    AddForumRoute.name: (routeData) {
+      return _i8.AdaptivePage<dynamic>(
+          routeData: routeData, child: const _i7.AddForumPage());
     }
   };
 
   @override
-  List<_i5.RouteConfig> get routes => [
-        _i5.RouteConfig(SplashRoute.name, path: '/'),
-        _i5.RouteConfig(HomeRoute.name, path: '/home-page'),
-        _i5.RouteConfig(MyProfileRoute.name, path: '/my-profile-page'),
-        _i5.RouteConfig(ForeignProfileRoute.name, path: '/foreign-profile-page')
+  List<_i8.RouteConfig> get routes => [
+        _i8.RouteConfig(SplashRoute.name, path: '/'),
+        _i8.RouteConfig(LoginRoute.name, path: '/login-page'),
+        _i8.RouteConfig(HomeRoute.name, path: '/home-page'),
+        _i8.RouteConfig(MyProfileRoute.name, path: '/my-profile-page'),
+        _i8.RouteConfig(ForeignProfileRoute.name,
+            path: '/foreign-profile-page'),
+        _i8.RouteConfig(ForumRoute.name, path: '/forum-page'),
+        _i8.RouteConfig(AddForumRoute.name, path: '/add-forum-page')
       ];
 }
 
 /// generated route for
 /// [_i1.SplashPage]
-class SplashRoute extends _i5.PageRouteInfo<void> {
+class SplashRoute extends _i8.PageRouteInfo<void> {
   const SplashRoute() : super(SplashRoute.name, path: '/');
 
   static const String name = 'SplashRoute';
 }
 
 /// generated route for
-/// [_i2.HomePage]
-class HomeRoute extends _i5.PageRouteInfo<void> {
-  const HomeRoute() : super(HomeRoute.name, path: '/home-page');
+/// [_i2.LoginPage]
+class LoginRoute extends _i8.PageRouteInfo<void> {
+  const LoginRoute() : super(LoginRoute.name, path: '/login-page');
+
+  static const String name = 'LoginRoute';
+}
+
+/// generated route for
+/// [_i3.HomePage]
+class HomeRoute extends _i8.PageRouteInfo<HomeRouteArgs> {
+  HomeRoute({_i9.Key? key})
+      : super(HomeRoute.name,
+            path: '/home-page', args: HomeRouteArgs(key: key));
 
   static const String name = 'HomeRoute';
 }
 
+class HomeRouteArgs {
+  const HomeRouteArgs({this.key});
+
+  final _i9.Key? key;
+
+  @override
+  String toString() {
+    return 'HomeRouteArgs{key: $key}';
+  }
+}
+
 /// generated route for
-/// [_i3.MyProfilePage]
-class MyProfileRoute extends _i5.PageRouteInfo<void> {
+/// [_i4.MyProfilePage]
+class MyProfileRoute extends _i8.PageRouteInfo<void> {
   const MyProfileRoute() : super(MyProfileRoute.name, path: '/my-profile-page');
 
   static const String name = 'MyProfileRoute';
 }
 
 /// generated route for
-/// [_i4.ForeignProfilePage]
-class ForeignProfileRoute extends _i5.PageRouteInfo<void> {
+/// [_i5.ForeignProfilePage]
+class ForeignProfileRoute extends _i8.PageRouteInfo<void> {
   const ForeignProfileRoute()
       : super(ForeignProfileRoute.name, path: '/foreign-profile-page');
 
   static const String name = 'ForeignProfileRoute';
+}
+
+/// generated route for
+/// [_i6.ForumPage]
+class ForumRoute extends _i8.PageRouteInfo<ForumRouteArgs> {
+  ForumRoute({_i9.Key? key})
+      : super(ForumRoute.name,
+            path: '/forum-page', args: ForumRouteArgs(key: key));
+
+  static const String name = 'ForumRoute';
+}
+
+class ForumRouteArgs {
+  const ForumRouteArgs({this.key});
+
+  final _i9.Key? key;
+
+  @override
+  String toString() {
+    return 'ForumRouteArgs{key: $key}';
+  }
+}
+
+/// generated route for
+/// [_i7.AddForumPage]
+class AddForumRoute extends _i8.PageRouteInfo<void> {
+  const AddForumRoute() : super(AddForumRoute.name, path: '/add-forum-page');
+
+  static const String name = 'AddForumRoute';
 }
