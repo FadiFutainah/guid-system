1a2ae399c2901f2c950b3574e072a0684fd5f954
suliman <sulimanawadstudy@gmail.com>
2022-08-17 23:36:37 +0300

    make the get forum request like a monsters

--- lib/application/features/all_forums/all_forums_cubit.dart
+++ lib/application/features/all_forums/all_forums_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:app/data/providers/forum_provider.dart';
 import 'package:app/domain/repositories/forum_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:app/data/entities/forum_dto.dart';
@@ -7,13 +8,33 @@ part 'all_forums_state.dart';
 
 class AllForumsCubit extends Cubit<AllForumsState> {
   AllForumsCubit() : super(AllFotumsInitial()) {
-    getForums();
+    getForums(
+      page: 1,
+      search: '',
+      ordering: Ordering.none,
+    );
   }
 
-  Future<void> getForums() async {
+  Future<void> getForums({
+    required int page,
+    required String search,
+    required Ordering ordering,
+    bool? isQuestion,
+    bool? isClosed,
+    bool? isMine,
+    int? tagID,
+  }) async {
     emit(Loading());
 
-    var response = await ForumRepository().getForums();
+    var response = await ForumRepository().getForums(
+      page: page,
+      search: search,
+      ordering: ordering,
+      isQuestion: isQuestion,
+      isClosed: isClosed,
+      isMine: isMine,
+      tagID: tagID,
+    );
 
     if (response.hasError) {
       emit(Failed());
--- lib/data/providers/forum_provider.dart
+++ lib/data/providers/forum_provider.dart
@@ -2,13 +2,53 @@ import 'package:app/data/entities/forum_dto.dart';
 import 'package:app/data/entities/reply_dto.dart';
 import 'package:app/data/helpers/restapi_service.dart';
 
+enum Ordering {
+  pointAsc,
+  pointDesc,
+  timeAsc,
+  timeDesc,
+  none,
+}
+
+Map<Ordering, String> orderingString = {
+  Ordering.pointAsc: 'points',
+  Ordering.pointDesc: '-points',
+  Ordering.timeAsc: 'time',
+  Ordering.timeDesc: '-time',
+  Ordering.none: '',
+};
+
 class ForumProvider {
   final RestApiService _restApi =
       RestApiService('https://guide-system-backend.herokuapp.com/api/forum/');
 
-  Future<List<ForumDto>> getForums() async {
+  Future<List<ForumDto>> getForums({
+    required int page,
+    required String search,
+    required Ordering ordering,
+    bool? isQuestion,
+    bool? isClosed,
+    bool? isMine,
+    int? tagID,
+  }) async {
+    String res = '?';
+    res += 'page=$page';
+    res += '&search=$search';
+    res += '&ordering=${orderingString[ordering]}';
+    if (isQuestion != null) {
+      res += '&is_question=$isQuestion';
+    }
+    if (isClosed != null) {
+      res += '&is_closed=$isClosed';
+    }
+    if (isMine != null) {
+      res += '&is_mine=$isMine';
+    }
+    if (tagID != null) {
+      res += '&tag=$tagID';
+    }
     try {
-      var response = await _restApi.get('forums/');
+      var response = await _restApi.get('forums/$res');
       return List<ForumDto>.from(
           response['results'].map((e) => ForumDto.fromMap(e)));
     } catch (e) {
@@ -25,7 +65,6 @@ class ForumProvider {
         ),
       );
     } catch (e) {
-      ;
       rethrow;
     }
   }
